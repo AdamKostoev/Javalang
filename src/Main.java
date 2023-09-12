e7e3bfbe44f21b9f917b95e365d3c57e4382f324
AdamKostoev <kosstoev.adam@gmail.com>
2023-09-12 10:57:43 +0300

    изменил код

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println("Сумма двух чисел равна " + sum);
+
+        String message = "Это новое изменение!";
+        System.out.println(message);
+
+
+    }
+}
